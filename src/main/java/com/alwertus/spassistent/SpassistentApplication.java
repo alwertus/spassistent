766b56d4bd8157b1e410bb2c5ebbe550b387cd07
Alwertus <alwertus@gmail.com>
2021-09-12 14:45:22 +0300

    test

--- src/main/java/com/alwertus/spassistent/SpassistentApplication.java
+++ src/main/java/com/alwertus/spassistent/SpassistentApplication.java
@@ -3,10 +3,21 @@ package com.alwertus.spassistent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+import java.io.IOException;
+
 @SpringBootApplication
 public class SpassistentApplication {
 
     public static void main(String[] args) {
+        String path = "~/testfile.txt";
+        File f = new File(path);
+        try {
+            f.createNewFile();
+        } catch (IOException e) {
+            System.out.println("ERROR! Cannot create testfile");
+            e.printStackTrace();
+        }
         SpringApplication.run(SpassistentApplication.class, args);
     }
 
