da2a6fd2ee44b9270998ff6f191e3a789bcb4ff2
Alwertus <alwertus@gmail.com>
2021-09-12 03:17:30 +0300

    Initial commit

---
+++ src/main/java/com/alwertus/spassistent/SpassistentApplication.java
@@ -0,0 +1,13 @@
+package com.alwertus.spassistent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpassistentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpassistentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alwertus/spassistent/SpassistentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alwertus.spassistent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpassistentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
