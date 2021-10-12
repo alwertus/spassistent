27981acd2c9f42d899276637c0a5ab585c3ed9df
Alwertus <alwertus@gmail.com>
2021-10-13 00:45:47 +0300

    cors

---
+++ src/main/java/com/alwertus/spassistent/config/CorsConfig.java
@@ -0,0 +1,16 @@
+package com.alwertus.spassistent.config;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@NoArgsConstructor
+@Data
+@ConfigurationProperties(prefix = "application.allowed")
+public class CorsConfig {
+    private String origins = "";
+    private String methods = "GET,POST";
+    private String headers = "Authorization,Cache-Control,Content-Type";
+}
--- src/main/java/com/alwertus/spassistent/config/SecurityConfig.java
+++ src/main/java/com/alwertus/spassistent/config/SecurityConfig.java
@@ -18,6 +18,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Log4j2
 @Configuration
@@ -26,16 +31,19 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserDetailsService userService;
     private final JwtRequestFilter jwtRequestFilter;
+    private final CorsConfig corsConfig;
 
     @Autowired
-    public SecurityConfig(UserDetailsService userService, JwtRequestFilter jwtRequestFilter) {
+    public SecurityConfig(UserDetailsService userService, JwtRequestFilter jwtRequestFilter, CorsConfig corsConfig) {
         this.userService = userService;
         this.jwtRequestFilter = jwtRequestFilter;
+        this.corsConfig = corsConfig;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors().and()
                 .csrf().disable()
                 .sessionManagement()
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // spring security do not use session state
@@ -79,5 +87,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
         daoAuthenticationProvider.setUserDetailsService(userService);
         return daoAuthenticationProvider;
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        if (corsConfig.getOrigins().isEmpty())
+            log.error("Parameter 'application.allowed.origins' is EMPTY");
+
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedOrigins(Arrays.asList(corsConfig.getOrigins().split(",")));
+        configuration.setAllowedMethods(Arrays.asList(corsConfig.getMethods().split(",")));
+        configuration.setAllowedHeaders(Arrays.asList(corsConfig.getHeaders().split(",")));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
