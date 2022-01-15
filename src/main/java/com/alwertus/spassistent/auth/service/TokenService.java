c75fc326e3dbec22edd601aba1537f2c15a080f4
alwertus <alwertus@gmail.com>
2022-01-16 01:37:56 +0300

    tmp

--- src/main/java/com/alwertus/spassistent/auth/service/TokenService.java
+++ src/main/java/com/alwertus/spassistent/auth/service/TokenService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Log4j2
@@ -20,15 +21,24 @@ public class TokenService {
 
     public String generateToken(String userName, String issuer, Collection<GrantedAuthority> userAuthorities, boolean isRefreshToken) {
         log.trace(String.format("Generate token with parameters: userName='%s', issuer='%s', isRefreshToken='%s'", userName, issuer, isRefreshToken));
-        return jwtProperties.getTokenPrefix() +
+
+        log.debug("-1");
+        Date expireDate = new Date(System.currentTimeMillis() + (long) (isRefreshToken ? 100 : 1) * jwtProperties.getTokenExpiration() * 60 * 1000);
+        log.debug("-2");
+        List<String> claim = userAuthorities.stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
+        log.debug("-3");
+
+        String result = jwtProperties.getTokenPrefix() +
                 JWT.create()
                 .withSubject(userName)
-                .withExpiresAt(new Date(System.currentTimeMillis() + (long) (isRefreshToken ? 100 : 1) * jwtProperties.getTokenExpiration() * 60 * 1000))
+                .withExpiresAt(expireDate)
                 .withIssuer(issuer)
-                .withClaim("roles",
-                        userAuthorities.stream()
-                                .map(GrantedAuthority::getAuthority)
-                                .collect(Collectors.toList()))
+                .withClaim("roles", claim)
                 .sign(jwtProperties.getAlgorithm());
+
+        log.debug("-4");
+        return result;
     }
 }
